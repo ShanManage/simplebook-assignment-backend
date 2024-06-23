8f33d3a94e63546a73c868c7f4b2e522c19c45e6
ShanManage <lakshan2015dk@gmail.com>
2024-06-24 00:59:30 +0530

    refactor create user API

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -17,6 +17,37 @@ export const getUser = async (req, res, next) => {
   }
 };
 
+export const createUser = async (req, res, next) => {
+  try {
+    const { firstName, lastName, email } = req.body;
+
+    if (!firstName) {
+      res.status(400).send({ message: 'First Name is required.!' });
+    }
+
+    if (!email) {
+      res.status(400).send({ message: 'E-mail is required.!' });
+    }
+
+    const userSnapshot = await db.collection('users').where('email', '==', email).get();
+    if (!userSnapshot.empty) {
+      return res.status(400).send({ message: 'E-mail already exists.!' });
+    }
+    
+    const data = {
+      firstName,
+      lastName,
+      email,
+      phone: '',
+      address: ''
+    }
+    await db.collection('users').add(data);
+    res.status(200).send('User created successfully');
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+};
+
 export const updateUser = async (req, res, next) => {
   try {
     const id = req.params.id;
