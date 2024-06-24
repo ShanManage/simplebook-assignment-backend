c4ced2e50e20bafd4b4ae52334b88683efb8c97d
ShanManage <lakshan2015dk@gmail.com>
2024-06-25 04:08:20 +0530

    feat: update profile image route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,24 @@
 import express from 'express'
+import multer from 'multer'
 import {
   getUser,
   createUser,
   updateUser,
+  updateProfileImage
 } from '../controllers/users.controller.js'
 
 const router = express.Router()
+
+const upload = multer({
+  storage: multer.memoryStorage()
+});
 
 router.get('/', getUser);
 
 router.post('/', createUser) 
 
-router.put('/:id', updateUser);
+router.put('/', updateUser);
+
+router.patch('/image', upload.single('image'), updateProfileImage);
 
 export default router
